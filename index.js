2c7546a838d8c48f012b2908619e3f8389773b74
warayuth <warayuth899@gmail.com>
2022-11-06 00:17:46 +0700

    Fix Resume and fix index

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    
+    console.log(element.classList.value);
+    if(element.classList.value === "dark-mode"){
+        localStorage.setItem("dark-mode",true);
+    }
+    else{
+      localStorage.removeItem("dark-mode");
+    }
  }
  function usernameStock() {
     var username = document.getElementById("username").value;
@@ -16,6 +24,47 @@ function darkMode() {
     }
 }
 function buttonEvent() {
+    alert("true");
+    // document.getElementById("A1").click();
+    // console.log("click Event buttom html");
+
+
+
+}    function loginTosever()
+ {
+    var username1 = document.getElementById("username").value;
+    var password1 = document.getElementById("password").value;
+    console.log( "username:",username1);
+    console.log(typeof (password1));  
+    $.ajax({
+  type: "POST",
+  // data: '{"username":"pupan"}, {"password":"1234"}',
+  data: {
+    username: username1,
+    password: password1,
+  },
+  url: "https://6b06-54-254-153-143.ap.ngrok.io/login",
+  cache: false,
+  success: function (data) {
+    console.log("object:",data.msg);
+    if (  data.msg == "success")
+    { 
+        console.log(username1);
+        window.location.replace("index.html");
+
+    }
+  },
+  error: function (data) {
+    console.log(data);
+    alert("false");
+  },
+
+//   var s = document.getElementById(variable);
+        // s.value = "new value";
+});
+}
+function buttonEvent() {
+    console.log(username)
     alert("true");
     // document.getElementById("A1").click();
     // console.log("click Event buttom html");
