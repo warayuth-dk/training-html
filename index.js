aaec0aaf41d3f4cd5127ef4092d5722f203be682
Pupan Pw <pupan.pou@cpmatch.com>
2022-12-17 22:46:00 +0700

    remove frontend --pupan

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
-
- function usernameStock() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if (username === "dukdik" && password === "123456") {
-        alert("Success");
-    }
-    else {
-        alert("fail");
-
-    }
-}
-function buttonEvent() {
-    alert("true");
-    // document.getElementById("A1").click();
-
-
-
-}    function loginTosever()
- {
-    var username1 = document.getElementById("username").value;
-    var password1 = document.getElementById("password").value;
-    $.ajax({
-  type: "POST",
-  // data: '{"username":"pupan"}, {"password":"1234"}',
-  data: {
-    username: username1,
-    password: password1,
-  },
-  url: "https://6b06-54-254-153-143.ap.ngrok.io/login",
-  cache: false,
-  success: function (data) {
-    if (  data.msg == "success")
-    { 
-        window.location.replace("index.html");
-
-    }
-  },
-  error: function (data) {
-    alert("false");
-  }
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send('Hello server is running')
+    .end();
+});
+ 
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
