cf7bdf594764aff4c205c571e8ca04c7da8a0dd0
warayuth <warayuth899@gmail.com>
2022-12-17 23:37:47 +0700

    add post login

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.get('/', (req, res) => {
     .json({"email": "dukdik_2003@hotmail.com", "password": "123456789er"})
     .end();
 });
+app.post("/login", (req, res) => {
+  var data = req.body();
+
+  if (data.email === "dukdik_2003@hotmail.com" && data.password === "123456789er") {
+    res.status(200).json({ msg: "login sucess", statusCode: 200 });
+  } else {
+    res.send("usernae or password incorrect");
+  }
+});
+
  
 // Start the server
 const PORT = process.env.PORT || 8080;
