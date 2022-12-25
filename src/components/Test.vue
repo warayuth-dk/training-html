2e864d09fd79360bd94d882d406463371a06aaac
Pupan Pw <pupan.pou@cpmatch.com>
2022-12-26 00:47:51 +0700

    vue init

---
+++ src/components/Test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- {{ this.pupan }} -->
+
+    <div v-for="value in objectName" :key="value.id">
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //   pupan: "hello duk",
+      header1: ["test", "helo", "tree"],
+      objectName: [
+        {
+          id: 1,
+          test: "hello duk",
+          test2: "ppppp",
+        },
+        {
+          id: 2,
+          test: "asdasd asdasd",
+          test2: "asdsadasdasd",
+        },
+      ],
+      arr: [123, 123123, 123123123213],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: ["vuetify"],
   // Add this line of code to disable lintOnSave
   lintOnSave: false,
 });
